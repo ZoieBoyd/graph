6234222aa13bc46d58f8d30f05484c744648ae1a
ZoieBoyd <zoieboyd@outlook.com>
2025-12-09 15:08:49 +0000

    feat: Add methods to graph class
    Added multiple methods the the graph class to support the implementation of the graph data structure.
    - Removing edges between nodes
    - Removing nodes
    - Displaying the nodes with retrospective neighbors in the console

--- graph.js
+++ graph.js
@@ -14,6 +14,44 @@ export class Graph {
             this.adjacencyList.get(startNode).push(endNode);
             this.adjacencyList.get(endNode).push(startNode);
         }
-        console.log(this.adjacencyList);
+    }
+
+    removeNode(node) {
+        for (let i = 0; i < this.nodes.length; i++) {
+            if (this.nodes[i] === node) {
+                this.nodes.splice(i, 1);
+                i--;
+            }
+        }
+
+        this.adjacencyList.delete(node);
+
+        this.adjacencyList.forEach((list) => {
+            if (list.includes(node)) {
+                list.splice(list.indexOf(node), 1);
+            }
+        });
+    }
+
+    removeEdge(start, end) {
+        if (!this.adjacencyList.has(start) || !this.adjacencyList.has(end))
+            return console.error("Error: One or both of these nodes do not exist.");
+
+        const startNode = this.adjacencyList.get(start);
+        const endNode = this.adjacencyList.get(end);
+
+        if (startNode.indexOf(end) === -1 || endNode.indexOf(start) === -1)
+            return console.error("Error: Edge does not exist between these nodes.");
+
+        startNode.splice(startNode.indexOf(end), 1);
+        endNode.splice(endNode.indexOf(start), 1);
+    }
+
+    display() {
+        let output = "";
+        this.adjacencyList.forEach((values, key) => {
+            output += key + " -> " + values.join(", ") + "\n";
+        });
+        console.log(output);
     }
 }
--- main.js
+++ main.js
@@ -2,7 +2,28 @@ import { Graph } from "./graph.js";
 
 const graph = new Graph();
 
-graph.addNode("hello");
-graph.addNode("bye");
-graph.addEdge("hello", "bye");
-graph.addEdge("hola", "adios");
+console.log("--- Social Network Simulation ---\n");
+
+console.log("Profiles created: ");
+graph.addNode("James");
+graph.addNode("Michael");
+graph.addNode("Lisa");
+graph.addNode("Jane");
+graph.addNode("Albert");
+graph.display();
+
+console.log("Friendships established: ");
+graph.addEdge("James", "Michael");
+graph.addEdge("James", "Lisa");
+graph.addEdge("James", "Jane");
+graph.addEdge("Michael", "Lisa");
+graph.addEdge("Jane", "Albert");
+graph.display();
+
+console.log("James unfriends Lisa: ");
+graph.removeEdge("James", "Lisa");
+graph.display();
+
+console.log("Michael deletes his profile: ");
+graph.removeNode("Michael");
+graph.display();
